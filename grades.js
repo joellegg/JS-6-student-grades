3dd28835dc95838f073a0f4d7f95016680e69328
joellegg <joellegg@bellsouth.net>
2016-12-02 09:25:38 -0600

    structure for js and html set up

--- grades.js
+++ grades.js
@@ -1,1 +1,24 @@
+// Start with array of random scores in your JavaScript
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var scores_a = "";
+var scores_b = "";
+// Loop over an array of student grades (values from 50-100) and outputs how many of each grades there are.
+for (var i = 0; i < scores.length; i++) {
+
+}
+// A score of 50-60 is an F
+
+// A score of 61-70 is a D
+
+// A score of 71-80 is a C
+
+// A score of 81-90 is a B
+
+// A score of 91-100 is an A
+
+
+// Use console.log to output the following criteria:
+
+// 1.	How many of each grade?
+// 2.	What is the lowest grade?
+// 3.	What is the highest grade?
